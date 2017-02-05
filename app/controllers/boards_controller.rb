56c1f7f6a2a175d18cba566e915d90ed343817fc
emre <emre@debian>
2017-02-05 15:34:57 +0300

    Added CRUD Operations git push origin master

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,39 @@
+class BoardsController < ApplicationController
+  def show
+      @board = Board.find(params[:id])
+  end
+
+  def index
+    @boards = Board.all
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  def board_params
+    params.require(:board).permit(:title, :description, :public)
+  end
+
+
+  def new
+    @board = Board.new
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    if Board.update(params[:id],board_params)
+      redirect_to boards_path notice: "Başarıyla Güncellendi!"
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get :boards , to: "boards#index"
+  get '/boards/new', to: 'boards#new'
+  post 'boards', to: 'boards#create'
+  get '/boards/:id/edit', to: 'boards#edit', as: :patch_board
+  get '/boards/:id', to: 'boards#show', as: :board
+  patch 'boards/:id', to: 'boards#update'
+
+
 end
